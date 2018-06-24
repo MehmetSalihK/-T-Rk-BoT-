a9577e77789dfd9e3fae7a97a3a333c0b76399f2
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-06-24 15:39:02 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,6 +1,13 @@
-const Discord = require("discord.js");
+"use strict";
 
-const TOKEN = "NDU5NzY1ODUyNTMxODUxMjY0.DhDwCg.oEp4JP2zagICLRcFgJSwF30fGA4";
+const Discord = require('discord.js');
+const config = require('./config.json');
+const disco = new Discord.Client();
+const prefix = config.prefix;
+const allowedUsers = config.allowedUsers;
+const roles = config.roleToDisco;
+
+const TOKEN = "NDU5NzY1ODUyNTMxODUxMjY0.DhEvVw.VizvMe7ve37Bms4P4rNTzFg6F-o";
 const PREFIX = "-";
 
 var bot = new Discord.Client();
@@ -594,4 +601,41 @@ bot.on("message", (message) => {
             }
         }
     });
+
+disco.on("message", message => {
+    
+      function discoRole() {
+        let random = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+        roles.forEach((role) => {
+          let theRole = message.guild.roles.find("name", role);
+          theRole.edit({color: random}).catch(e => {
+            return message.channel.sendMessage(":x: **Error:** Belirttiğiniz rol `config.json` bu sunucuda bir rol değil, veya onun sahip olduğum en yüksek rolden daha yüksek bir rolü.");
+          });
+        });
+      }
+    
+      if(message.content.startsWith(prefix + "startpavyon")) {
+        if(allowedUsers.includes(message.author.id)) {
+        setInterval(() => { discoRole(); }, config.ms);
+        message.channel.sendMessage("```css\nPavyon Başlasın!...```");
+        message.channel.sendMessage(" ");
+      } else {
+        message.reply(` `);
+      }
+    } else
+    
+    if(message.content.startsWith(prefix + "stoppavyon")) {
+      if(allowedUsers.includes(message.author.id)) {
+      message.channel.sendMessage("Pavyon Bitti.");
+      setTimeout(() => { console.log(process.exit(0)); }, 300);
+    } else {
+      message.reply(` `);
+      }
+    }
+    
+    });
+
+    disco.login(config.token);
+disco.login(config.token);
+
 bot.login(TOKEN);
