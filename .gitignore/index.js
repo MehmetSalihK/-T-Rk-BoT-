ac39074df5e580c1575409d6a691bea39b7ec95c
MehmetSalihK <37173211+MehmetSalihK@users.noreply.github.com>
2018-06-26 09:18:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -27,47 +27,47 @@ bot.on("message", message => {
   }
 });
 
-const CLEAR_MESSAGES = '-clearchat';
+const CLEAR_MESSAGES = '!clearchat';
 
 bot.on('ready', () => {
-    console.log('Mesajları silmeye hazırım!');
-    bot.on('message', message => {
-      if (message.content == CLEAR_MESSAGES) {
-  
-        // Check the following permissions before deleting messages:
-        //    1. Check if the user has enough permissions
-        //    2. Check if I have the permission to execute the command
-  
-        if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
-          message.channel.sendMessage("Üzgünüz, komutu yürütme izniniz yok \""+message.content+"\"");
-          console.log("Üzgünüz, komutu yürütme izniniz yok \""+message.content+"\"");
-          return;
-        } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
-          message.channel.sendMessage("Üzgünüm, komutu yürütme iznim yok \""+message.content+"\"");
-          console.log("Üzgünüm, komutu yürütme iznim yok \""+message.content+"\"");
-          return;
-        }
-  
-        // Only delete messages if the channel type is TextChannel
-        // DO NOT delete messages in DM Channel or Group DM Channel
-        if (message.channel.type == 'text') {
-          message.channel.fetchMessages()
-            .then(messages => {
-              message.channel.bulkDelete(messages);
-              messagesDeleted = messages.array().length; // number of messages deleted
-  
-              // Logging the number of messages deleted on both the channel and console.
-              message.channel.sendMessage("Mesajları başarılı bir şekilde silme. Silinen mesajların toplam sayısı: "+messagesDeleted).then(d_msg => { d_msg.delete(30000); });
-              console.log('Mesajları başarılı bir şekilde silme. Silinen mesajların toplam sayısı: '+messagesDeleted).then(d_msg => { d_msg.delete(30000); });
-            })
-            .catch(err => {
-              console.log('Toplu silme sırasında hata oluştu');
-              console.log(err);
-            });
-        }
+  console.log('ClearMessagesBot is Ready!');
+  bot.on('message', message => {
+    if (message.content == CLEAR_MESSAGES) {
+
+      // Check the following permissions before deleting messages:
+      //    1. Check if the user has enough permissions
+      //    2. Check if I have the permission to execute the command
+
+      if (!message.channel.permissionsFor(message.author).hasPermission("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
+        console.log("Sorry, you don't have the permission to execute the command \""+message.content+"\"");
+        return;
+      } else if (!message.channel.permissionsFor(bot.user).hasPermission("MANAGE_MESSAGES")) {
+        message.channel.sendMessage("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
+        console.log("Sorry, I don't have the permission to execute the command \""+message.content+"\"");
+        return;
       }
-    });
+
+      // Only delete messages if the channel type is TextChannel
+      // DO NOT delete messages in DM Channel or Group DM Channel
+      if (message.channel.type == 'text') {
+        message.channel.fetchMessages()
+          .then(messages => {
+            message.channel.bulkDelete(messages);
+            messagesDeleted = messages.array().length; // number of messages deleted
+
+            // Logging the number of messages deleted on both the channel and console.
+            message.channel.sendMessage("Deletion of messages successful. Total messages deleted: "+messagesDeleted);
+            console.log('Deletion of messages successful. Total messages deleted: '+messagesDeleted)
+          })
+          .catch(err => {
+            console.log('Error while doing Bulk Delete');
+            console.log(err);
+          });
+      }
+    }
   });
+});
 
 bot.on("guildMemberAdd", function(member) {
     
